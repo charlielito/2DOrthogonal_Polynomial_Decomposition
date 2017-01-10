b47f048e5870c4d7ec296a25a2a6b41a2d216e92
charlielito <charlie_cha@outlook.com>
2017-01-10 16:03:22 -0500

    Add files via upload

--- mainGFRPsample.m
+++ mainGFRPsample.m
@@ -7,12 +7,12 @@ path(path,[currdir(1:sep(end)),'functions']);
 
 
 disp('-------- Loading data... ------')
-load GFRPsample RTfront tfront RTrear trear
+load GFRPsample %RTfront tfront RTrear trear
 disp('-------- Data loaded! ------')
 
-[m,n,p] = size(RTfront);
+[m,n,p1] = size(RTfront);
 [m2,n2,p2] = size(RTrear);
-p = min([p p2]);
+p = min([p1 p2]);
 
 % Front Sample defects Centers
 mc = [71   152   228   305   383];
@@ -33,7 +33,7 @@ ls = [3 15 3 15 3;
       10 5 10 5 10;
       15 3 15 3 15]; %sizes: 3 smallest one
                        %    15 biggest one                     
-kk = 1.5;
+kk = 1.5; %windowsize/flawsize ratio
 mls = kk*round(ls*d/50);
 pmn = round(mls/2); %polynomial order
 %pmn = mls; 
@@ -75,11 +75,24 @@ nf2 = nc2 + round(0.5*mls);
 K0mn = zeros(5,5,p);
 K0mn2 = zeros(5,5,p);
 
-disp('-------- Calculating OPD for all defects... ------')
-for pp = 1:p
+PCT = zeros(5,5,p1);
+PCT2 = zeros(5,5,p2);
 
-for mm = 1:5
-    for nn = 1:5
+disp('-------- Calculating TSR ... (this may take some minutes) ------')
+FTfront = TSR(RT_coefffront,tfront,9,'FT');
+disp('-------- TSR front terminated! ... ------')
+FTrear = TSR(RT_coeffrear,trear,9,'FT');
+disp('-------- TSR terminated! ... ------')
+
+disp('-------- Calculating OPD for biggest defects... ------')
+h = waitbar(0,'Calculating OPD for biggest defects...');
+steps = p;
+coord=[5 1; 1 2; 5 3; 1 4; 5 5]; %only the biggest defects
+for pp = 1:p
+waitbar(pp / steps)
+    for coor = coord'
+        mm = coor(1);
+        nn = coor(2);
         intM = mi(mm,nn):mf(mm,nn);
         intN = ni(mm,nn):nf(mm,nn);
         intM2 = mi2(mm,nn):mf2(mm,nn);
@@ -92,11 +105,10 @@ for mm = 1:5
         Coeffmn2 = OPD((Matrix_mn2),[pmn2(mm,nn) pmn2(mm,nn)]);
         K0mn(mm,nn,pp) = (mean(abs(Coeffmn(3,:))) + mean(abs(Coeffmn(:,3))))/2;
         K0mn2(mm,nn,pp) = (mean(abs(Coeffmn2(3,:))) + mean(abs(Coeffmn2(:,3))))/2;
+
     end
 end
-
-end
-
+close(h)
 disp('-------- OPD calculated! ------')
 %%
 disp('-------- Calculating Normalized Contrast... ------')
@@ -104,10 +116,42 @@ NCfront = ntc(RTfront,tfront,5,206,371,mc,nc);
 disp('-------- Normalized Contrast for front part terminated! ------')
 NC = ntc(RTrear,trear,5,370,350,mc2,nc2); %for rear sample
 disp('-------- Normalized Contrast for REAR part terminated! ------')
+disp('-------- Fitting and ploting... ------')
+%%
 
+%%
+disp('-------- Calculating PCT for biggest defects... ------')
+h = waitbar(0,'Calculating PCT for biggest defects...');
+i = 0;
+for coor = coord'
+    mm = coor(1);
+    nn = coor(2);
+    waitbar(i / length(coord))
+    intM = mi(mm,nn):mf(mm,nn);
+    intN = ni(mm,nn):nf(mm,nn);
+    intM2 = mi2(mm,nn):mf2(mm,nn);
+    intN2 = ni2(mm,nn):nf2(mm,nn);
+
+    Matrix_mn = squeeze(FTfront(intM,intN,:));
+    Matrix_mn2 = squeeze(FTrear(intM2,intN2,:));
+    PCA_Mat = compress3DTo2DForPCA(Matrix_mn);
+    [~,~,V] = svd(PCA_Mat);
+    PCT(mm,nn,:) = V(:,2)'; %time profile from 2nd orthogonal basis
+
+    PCA_Mat = compress3DTo2DForPCA(Matrix_mn2);
+    [~,~,V] = svd(PCA_Mat);
+    PCT2(mm,nn,:)  =  V(:,2)'; %time profile from 2nd orthogonal basis
+    i = i +1;
+end
+waitbar(i / length(coord))
+close(h)
+disp('-------- PCT calculated! ------')
 
 
 %%
+Tsfront = fitSoundA(RTfront,tfront,5,100,380); %sound areas fitted for PPT
+Tsrear = fitSoundA(RTrear,trear,5,90,360);
+
 fs = 1/55; %sample period 
 
 Sm = [1 5 1 5 1;   % Position according to sizes
@@ -130,10 +174,16 @@ NCbigFront = [];
 NCbigBack = [];
 RTbigFront = [];
 RTbigBack = [];
+PCTFront = [];
+PCTBack = [];
 for nn = 1:5
     %OPD
     CoeffsFront = [CoeffsFront; squeeze(K0mn( Sm(mm,Hn(nn)), Hn(nn), :))'];
     CoeffsBack = [CoeffsBack; squeeze(K0mn2( Sm(mm,Hn2(nn)), Hn2(nn), :))'];
+    
+    %PCT
+    PCTFront = [PCTFront; squeeze(PCT( Sm(mm,Hn(nn)), Hn(nn), :))'];
+    PCTBack = [PCTBack; squeeze(PCT2( Sm(mm,Hn2(nn)), Hn2(nn), :))'];
     
     %NTC
     NCbigFront = [NCbigFront; squeeze(NCfront( Sm(mm,Hn(nn)), Hn(nn), :))'];
@@ -150,28 +200,41 @@ CoefTotal = [CoeffsFront(:,1:N); CoeffsBack(:,1:N)];
 Nn = min([length(NCbigFront), length(NCbigBack)]);
 NCbig = [NCbigFront(:,1:Nn); NCbigBack(:,1:Nn)];
 
+Nn = min([length(PCTFront), length(PCTBack)]);
+PCTTotal = [PCTFront(:,1:Nn); PCTBack(:,1:Nn)];
 
 Nn = min([length(RTbigFront), length(RTbigBack)]);
 RTbig = [RTbigFront(:,1:Nn); RTbigBack(:,1:Nn)];
 
+tall = tfront(1:Nn);
+% PPT Parameters
+[~, frames] = size(RTbig);
+Fs = 55;
+nppt = 1;
+Tsfront = Tsfront(1:nppt:frames);
+Tsrear = Tsrear(1:nppt:frames);
+Ts = [Tsfront;Tsfront;Tsfront;Tsfront;Tsfront;Tsrear;Tsrear;Tsrear;Tsrear;Tsrear];
+
 depths = [0.2:0.2:1 1:0.2:1.8];
 
 N = length(CoefTotal(1,:));
-t5 = log((1:N)*fs);
+t4 = log((1:N)*fs);
 Ndefect = 10;
 tGauss = zeros(1,Ndefect);
 
-
+frames2 = size(NCbig,2);
+logt = log(tall);
 f = size(RTbig,2);
 t = log((1:f)*fs);
 t2 = 1:f;
 t3 = log(1:f);
 times = zeros(1,10);
 times2 = zeros(1,10);
-
-f = size(NCbig,2);
-
+times4 = zeros(1,10);
+times5 = zeros(1,10);
+t5=logt;
 for i = 1:Ndefect
+    % ********************* OPD *****************************
     figure(1)
     subplot(2,5,i)
     C = CoefTotal(i,:);
@@ -183,10 +246,10 @@ for i = 1:Ndefect
     title(['Depth: ',num2str(depths(i)),' mm'])
     xlim([min(t5) max(t5) ])
     
-    
+    % ********************** NTC **************************
     figure(2)
     subplot(2,5,i)
-    t = log((1:f)*fs);
+    t = log((1:frames2)*fs);
     t = t(1:end);
     ncc = NCbig(i,1:end);
     f1 = polyfit(t',ncc',7);
@@ -195,7 +258,7 @@ for i = 1:Ndefect
     [Max, Indx] = findpeaks(double(pNC));
      if (~isempty(Indx))
      times2(i) = fs*Indx( Max == max(Max(Max<Inf) ) );
-     if i ==6 times2(i) = Indx(2)*fs;end %original was Indx(2)
+     %if i ==6 times2(i) = Indx(2)*fs;end %original was Indx(2)
      end
 
      plot(t,pNC,'k',t(times2(i)/fs),pNC(times2(i)/fs),'xr')
@@ -214,7 +277,7 @@ for i = 1:Ndefect
     DTdef = polyval(p,t3);
 
     
-    
+    % ***************** TSR ****************************
     figure(3)
     subplot(2,5,i)
     p = diffPoly(p);
@@ -228,28 +291,79 @@ for i = 1:Ndefect
     title(['Depth: ',num2str(depths(i)),' mm'])
     xlabel('ln(t)')
     ylabel('d^2ln(T)/d[ln(t)] ^2')
-    xlim([min(log((1:f)*fs)) max(t3) ])
+    xlim([min(log((1:frames2)*fs)) max(t3) ])
+    
+    %************ PCT ************
+    figure(4)
+    subplot(2,5,i)
+    C = PCTTotal(i,:);
+    DataInv = 1.01*max(C) - C;
+    [Minima,MinIdx] = findpeaks(DataInv);
+    plot(logt,C,'k')
+    xlabel('ln(t)')
+    ylabel('2nd Orthogonal Vector')
+    hold on
+    title(['Depth: ',num2str(depths(i)),' mm'])
+    xlim([min(logt) max(logt) ])
+    if ~isempty(MinIdx) 
+        times4(i) = MinIdx(end);
+        plot(logt(MinIdx(end)), C(MinIdx(end)),'rx')
+    end
+    
+    
+    %********** PPT *****************
+    figure(5)
+    Tdef = RTbig(i,:);        
+    y = log(Tdef);
+    ypol = polynomialfit(logt(:),y(:),9);
+    yfit = polyval(ypol,logt);
+    Tdef2 = exp(yfit(1:nppt:frames));
+
+    TdefFreq = fft(Tdef2);
+    TsFreq = fft(Ts(i,:));
+    Frames = size(TdefFreq,2);
+
+    TdefFreq = TdefFreq(1:floor(Frames/2)+1);
+    TsFreq = TsFreq(1:floor(Frames/2)+1);%only positive frequencies
+    f = (0:Frames/2)*Fs/Frames/nppt;
+    Pdef = angle(TdefFreq); %phase profiles
+    Psound = angle(TsFreq);
+    PCon = Psound-Pdef; %phase contrast
+    factor = 10; %resampling factor to have better blind frequency estimation
+    PCon2 = resample(double(PCon), factor,1);
+    f2 = (0:length(PCon2)-1)*Fs/Frames/nppt/factor; %frequency resampled
+    times5(i) = f2(zerocrossPPT(PCon2(2:end))); %calculate zero cross (blind f_b)
+    %times(i) = f(zerocrossPPT(PCon(2:end))); %calculate zero cross (blind f_b)
+    plot(f,PCon)
+    xlabel('Frequency (Hz)')
+    ylabel('Phase Contrast (rad)')
+    hold all
+    title('Phase contrasts for different dephts ')
+    xlim([min(f) max(f) ])
 end
+figure(5),legend('0.2 mm', '0.4 mm', '0.6 mm','0.8 mm','1.0 mm','1.0 mm','1.2 mm','1.4 mm','1.6 mm','1.8 mm')
+
 %%
+% ***** TSR *********************
 depths = 0.2:0.2:1;
 limit = 3;
 t1 = times(1:limit);
-figure(4), plot(t1, depths(1:limit),'xr', 'linewidth',1.5,'MarkerSize',10), hold on
+figure(6), plot(t1, depths(1:limit),'xr', 'linewidth',1.5,'MarkerSize',10), hold on
 
 degree = 1;
 tf = 2.6;
 [p,s] = polyfit(t1', depths(1:limit)',degree);
-t2 = linspace(t1(1)-0.2,tf);
+t2 = linspace(t1(1)-0.4,tf);
 z = polyval(p,t2);
-h1 = plot(t2,z,'r');
-R = zeros(1,3);
+h1 = plot(t2,z,'r','linewidth',1.5);
+R = zeros(1,5);
 R(1) =  (1 - s.normr^2/norm(depths(1:limit)-mean(depths(1:limit)))^2)*100;
-Zestimated = zeros(3,length(depths));
+Zestimated = zeros(5,length(depths));
 Zestimated(1,1:length(t1)) = polyval(p,t1);
 p_tsr = p;
 
-
-tf = 6.5;
+% ********  NTC ***********
+tf = 8.5;
 linear = 0; %linear fit or not linear (sqrt(x))
 limit = 5;
 t1 = times2(1:limit);
@@ -260,60 +374,135 @@ if (linear)
     z = polyval(p,t2);
     R(2) =  (1 - s.normr^2/norm(depths-mean(depths))^2)*100;
     Zestimated(2,1:length(t1)) = polyval(p,t1);
+    p_ntc = p;
 else
     Eqn = 'a*sqrt(x) + b';
     [f1, gof] = fit(t1',depths',Eqn);
     z = f1.a*sqrt(t2)+f1.b;
     R(2) = gof.rsquare*100;
     Zestimated(2,1:length(t1)) = f1.a*sqrt(t1)+f1.b;
+    p_ntc = [f1.a,f1.b];
 end
 hold on
 h2 = plot(t2,z,'.-.b','linewidth',1.5,'markersize',3);
-p_ntc = f1;
 
-
-tf = 6.5;
+% ************* OPD **********
+tf = 8.5;
 linear = 0; %linear fit or not linear (sqrt(x))
 t1 = [tGauss(1:4), tGauss(6)];
 plot(t1, depths,'+k', 'linewidth',1.5,'MarkerSize',8)
-t2 = linspace(0.2,tf);
+t2 = linspace(0,tf);
 if (linear)
     [p,s] = polyfit(t1', depths',1);
     z = polyval(p,t2);
     R(3) =  (1 - s.normr^2/norm(depths-mean(depths))^2)*100;
     Zestimated(3,1:length(t1)) = polyval(p,t1);
+    p_opd = p;
 else
     Eqn = 'a*sqrt(x) + b';
     [f1, gof] = fit(t1',depths',Eqn);
     z = f1.a*sqrt(t2)+f1.b;
     R(3) = gof.rsquare*100;
     Zestimated(3,1:length(t1)) = f1.a*sqrt(t1)+f1.b;
+    p_opd = [f1.a,f1.b];
+
 end
 hold on
 h3 = plot(t2,z,':k','linewidth',2);
-p_opd = f1;
 
 
 
+% ********* PCT **********
+limit = 4;
+depths = depths(1:limit);
+linear = 1; %linear fit or not linear (sqrt(x))
+times4t = [times4(1:4), times4(6:7)];
+t1 = times4t(1:limit)*fs;
+plot(t1, depths,'*g', 'linewidth',1.5,'MarkerSize',7)
+t2 = linspace(0,9);
+if (linear)
+    [p,s] = polyfit(t1', depths',1);
+    z = polyval(p,t2);
+    R(4) =  (1 - s.normr^2/norm(depths-mean(depths))^2)*100;
+    Zestimated(4,1:length(t1)) = polyval(p,t1);
+    p_pca = p;
+else
+    Eqn = 'a*sqrt(x) + b';
+    [f2, gof] = fit(t1',depths',Eqn,'Startpoint',[1 1]);
+    z = f2.a*sqrt(t2)+f2.b;
+    R(4) = gof.rsquare*100;
+    Zestimated(4,1:length(t1)) = f2.a*sqrt(t1)+f2.b;
+    p_pca(1) = f2.a;
+    p_pca(2) = f2.b;
+end
+hold on
+h4 = plot(t2,z,'--g','linewidth',1.5,'markersize',3);
 
+
+% **** PPT *************
+depths = 0.2:0.2:1.8;
+times5t = [times5(1:4), times5(6:end)];
+range = 1:5;
+t = times5t(range).^(-1);
+plot(t, depths(range),'^m', 'linewidth',1.5,'MarkerSize',7)
+[f1,gof] = fit(t',depths(range)','a*sqrt(x) + b');
+t2 = linspace(0,9);
+z = f1.a*sqrt(t2) + f1.b;
+h5 = plot(t2,z,'m');
+R(5) =  gof.rsquare*100;
+p_ppt = [f1.a f1.b];
+Zestimated(5,1:length(times5t)) = p_ppt(1)*sqrt(times5t.^-1) + p_ppt(2);
+
+
+%round to 1 and 2 decimals
+R = (round(10*R)/10);
+p_pca = (round(1000*p_pca)/1000);
+p_tsr = (round(1000*p_tsr)/1000);
+p_ntc = (round(1000*p_ntc)/1000);
+p_opd = (round(1000*p_opd)/1000);
+p_ppt = (round(1000*p_ppt)/1000);
+
+% ----Place equations and errors R^2
+signn = containers.Map({1, -1},{'+','-'});
 depths = 0.6;
 t4text = max(max([times2,t1]))/2+1;
-text(t4text,depths(end)-0.2,['R^2 = ', num2str(R(3)), ' %'],'Color','black')
-text(t4text,depths(end)-0.1,['R^2 = ', num2str(R(2)), ' %'],'Color','blue')
-text(t4text,depths(end),['R^2 = ', num2str(R(1)), ' %'],'Color','red')
-legend([h1 h2 h3],'TSR','NTC', 'OPD','Location','northwest')
-set(get(gca,'xlabel'),'string','Specific characteristic time (s)','fontsize',12)
-set(get(gca,'ylabel'),'string','Depth (mm)','fontsize',12)
+
+text(t4text,depths(end)+0.05,['R^2_{TSR} = ', num2str(R(1)), ' %'],'Color','red','FontSize',8)
+text(t4text,depths(end),['z_{TSR} = ', num2str(p_tsr(1)), 't ', ...
+    signn(sign(p_tsr(2))),' ', num2str(abs(p_tsr(2)))],'Color','red','FontSize',8)
+
+text(t4text,depths(end)-0.1,['R^2_{NTC} = ', num2str(R(2)), ' %'],'Color','blue','FontSize',8)
+text(t4text,depths(end)-0.15,['z_{NTC} = ', num2str(p_ntc(1)), 't^{1/2} ', ...
+    signn(sign(p_ntc(2))),' ',  num2str(abs(p_ntc(2))) ],'Color','blue','FontSize',8)
+
+text(t4text,depths(end)-0.25,['R^2_{OPD} = ', num2str(R(3)), ' %'],'Color','black','FontSize',8)
+text(t4text,depths(end)-0.3,['z_{OPD} = ', num2str(p_opd(1)), 't^{1/2} ', ...
+    signn(sign(p_opd(2))),' ',  num2str(abs(p_opd(2))) ],'Color','black','FontSize',8)
+
+text(t4text,depths(end)-0.4,['R^2_{PCT} = ', num2str(R(4)), ' %'],'Color','g','FontSize',8)
+text(t4text,depths(end)-0.45,['z_{PCT} = ', num2str(p_pca(1)), 't ', ...
+    signn(sign(p_pca(2))),' ',  num2str(abs(p_pca(2))) ],'Color','g','FontSize',8)
+
+text(t4text-1.7,depths(end)-0.4,['R^2_{PPT} = ', num2str(R(5)), ' %'],'Color','m','FontSize',8)
+text(t4text-1.7,depths(end)-0.45,['z_{PPT} = ', num2str(p_ppt(1)), 't^{1/2} ', ...
+    signn(sign(p_ppt(2))),' ',  num2str(abs(p_ppt(2))) ],'Color','m','FontSize',8)
+
+
+legend([h1 h2 h3 h4 h5],'TSR','NTC', 'OPD','PCT', 'PPT','Location','northwest')
+set(get(gca,'xlabel'),'string','Specific characteristic time t (s)','fontsize',12)
+set(get(gca,'ylabel'),'string','Depth z (mm)','fontsize',12)
 set(get(gca,'title'),'string','Depth estimation','fontsize',11, 'FontWeight','bold')
+axis( [0 7.2 0.01 1.2])
 
 depths = 0.2:0.2:1;
-Zestimated;
-Zexact = [depths;depths;depths];
-Zerror = (100*abs(Zestimated-Zexact)./Zexact);
+Zestimated = Zestimated(:,1:length(depths));
+Zexact = [depths;depths;depths;depths;depths];
+Zerror = (100*(round(100*Zestimated)/100-Zexact)./Zexact);
+Zerror = Zerror(:,1:5);
 clc
 disp('*********** Predicted values (mm) *******')
-disp('Value     TSR         NTC         OPD')
-disp(num2str([depths' Zestimated(:,1:5)']))
-disp('*********** Relative errors (%) *******')
-disp('  TSR           NTC           OPD')
-disp(num2str(Zerror(:,1:5)'))
+disp('Value        TSR        NTC         OPD         PCT         PPT')
+disp(num2str([depths' Zestimated(:,1:5)'],'%0.2f        '))
+disp('*********** Relative errors (%) *********')
+disp('            TSR           NTC         OPD          PCT          PPT')
+disp(num2str([nan*ones(1,size(Zerror,2))' Zerror'],'%0.1f       '))
--- mainSyntheticCFRP.m
+++ mainSyntheticCFRP.m
@@ -8,20 +8,19 @@ path(path,[currdir(1:sep(end)),'functions']);
 
 
 disp('-------- Loading data... ------')
-load syntheticCFRP %RT and t and RT_coeffall and n
+load syntheticCFRP %RT and t
 disp('-------- Data loaded! ------')
 disp('-------- Calculating things... ------')
 
 mc = [250 50  50  250 250 50 50  250 250]; %1 pixel approx 1mm
 nc = [150 200 100 250 50  50 250 100 200]; 
-Tmat = RT;
 P = size(RT,3);
 N = length(mc);
 fs = 1/0.018;
 logt = log((1:P)/fs);
 
 
-ls = 15*1.5; %long side of window
+ls = 15*2; %long side of window
 
 %For POD windows
 mi = mc - round(ls/2);
@@ -32,7 +31,7 @@ K0mn = zeros(1,P);
 
 ls = round(ls);
 
-figure(1),imagesc(RT(:,:,400))
+figure(1),imagesc(RT(:,:,150))
 
 ms = 234;
 ns = 170;
@@ -47,6 +46,7 @@ for j = 1:win
 end
 hold off
 axis off
+axis square
 
 Ts = Ts/(win*win);
 
@@ -55,13 +55,22 @@ Ts0 = Ts/Ts(1);
 times = zeros(1,N);
 times2 = times;
 times3 = times;
+times4 = times;
+times5 = times;
+times3_2 = times;
 depths = 0.2:0.2:1.8;
 
+%PPT parameters
+[rows, cols, frames] = size(RT);
+Fs = 55;
+nppt = 1;
+Tsppt = Ts;
 
 for i = 1:N
+    % ****** NTC ***********
     figure(2)
     subplot(2,5,i)
-    Tdef = squeeze(Tmat(mc(i), nc(i),:))./squeeze(Tmat(mc(i), nc(i),1)) - Ts0;
+    Tdef = squeeze(RT(mc(i), nc(i),:))./squeeze(RT(mc(i), nc(i),1)) - Ts0;
     
     temp = find(Tdef == max(Tdef));
     if temp/fs < exp(logt(end-10)) %there is no right contrast peak
@@ -71,14 +80,13 @@ for i = 1:N
     xlabel('ln(t)')
     ylabel('C^n(t)')
     xlim([min(logt) max(logt) ])
-    %ylim([max(Tdef)*0.5 max(Tdef)*1.1 ])
 
-    
+    % ****** TSR ***********
     %Calculate 1st derivatives
     n = 1;
     logt = log((1:P)/fs);
     logt = logt(n:end);
-    Tdef = log(squeeze(Tmat(mc(i), nc(i),n:end)));
+    Tdef = log(squeeze(RT(mc(i), nc(i),n:end)));
     Ts = log(Ts0(n:end));
     p = polyfit(logt',Tdef,7);
     Tdefp = polyval(p,logt);
@@ -95,66 +103,124 @@ for i = 1:N
     DDTdef = polyval(p,logt);
     ps = diffPoly(ps);
     DDTs = polyval(ps,logt);
-    [Max, Idx] = findpeaks(DDTdef);
+    [Max, Idx] = findpeaks(DDTdef,'SORTSTR','ascend');
    
     if ~isempty(Idx) times(i) = Idx(1);
     else
     Idx = 1;Max = DDTdef(Idx); end
-    plot(logt,DDTdef,'k',logt(Idx), Max, 'xr')
+    plot(logt,DDTdef,'k',logt(Idx), Max, 'xr'), hold all
     title(['Depth: ',num2str(depths(i)),' mm'])
     xlabel('ln(t)')
     ylabel('d^2ln(T)/d[ln(t)] ^2')
     xlim([min(logt) max(logt) ])
-    ylim([-0.05 0.05])
+    ylim([-0.05 0.09])
 
-    % Calculate OPD
-    logt = log((1:P)/fs);
-    figure(4)
-    subplot(2,5,i)
+    % --------------- Calculate OPD and PCT
     intM = mi(i):mf(i);
     intN = ni(i):nf(i);
-
+    logt = log((1:P)/fs);
     
+    % *****PCT **********
+    figure(4)
+    subplot(2,5,i)
+    Matrix_mn = RT(intM,intN,:);
+    PCA_Mat = compress3DTo2DForPCA(Matrix_mn);
+    [U,S,V] = svd(PCA_Mat);
+    x = V(:,2)'; %time profile from 2nd orthogonal basis
+    [Max, Idx] = findpeaks(x,'SORTSTR','ascend','minpeakdistance',100);
+    DataInv = 1.01*max(x) - x;
+    [Minima,MinIdx] = findpeaks(DataInv,'SORTSTR','ascend','minpeakdistance',100);
+    plot(logt,x,'k')
+    xlabel('ln(t)')
+    ylabel('2nd Orthogonal Vector')
+    hold on
+    title(['Depth: ',num2str(depths(i)),' mm'])
+    xlim([min(logt) max(logt) ])
+    if isempty(Max) || Idx(end) < 15 || Idx(end) > 300% no maxima or fake maxima
+        times4(i) = MinIdx(end);
+        plot(logt(MinIdx(end)), x(MinIdx(end)),'rx')
+    else
+        times4(i) = Idx(end);
+        plot(logt(Idx(end)),Max(end),'rx')
+    end
+    hold off
+
+    % **** OPD ************
+    figure(5)
+    subplot(2,5,i)
     for pp = 1:P
         Matrix_mn = squeeze(RT(intM,intN,pp));
         Coeffmn = OPD((Matrix_mn),[ls ls]);
         K0mn(pp) = (mean(abs(Coeffmn(3,:))) + mean(abs(Coeffmn(:,3))))/2;
 
     end
-    [fpol, fgauss, tMin, tMax ] = remove_tendency(K0mn,10);
+    [fpol, fgauss, tMin, tMax ] = remove_tendency(K0mn,12);
     plot(logt,log(K0mn)-min(log(K0mn)),'k',logt,fgauss,'-.k',logt(tMax),fgauss(tMax),'xr')
+    %plot(logt,fgauss)
+    hold all
     title(['Depth: ',num2str(depths(i)),' mm'])
     xlabel('ln(t)')
     ylabel('ln(Coeff)')
     xlim([min(logt) max(logt) ])
     times3(i) = tMax;
+    [~, Index] = findpeaks(fpol,'SORTSTR','ascend');
+    if ~isempty(Index)    times3_2(i) = Index(end); end
 
+    
+% ******** PPT ********************
+    figure(6)
+    Temp = squeeze(RT(mc(i), nc(i),1:nppt:frames));
+    TdefFreq = fft(Temp);
+    TsFreq = fft(Tsppt(1:nppt:frames));
+    Frames = size(TdefFreq,1);
+    TdefFreq = TdefFreq(1:floor(Frames/2)+1);%only positive frequencies
+    TsFreq = TsFreq(1:floor(Frames/2)+1);%only positive frequencies
+    f = (0:Frames/2)*Fs/Frames/nppt;
+    Pdef = angle(TdefFreq); %phase profiles
+    Psound = angle(TsFreq);
+    PCon = Psound-Pdef; %phase contrast
+    factor = 10; %resampling factor to have better blind frequency estimation
+    PCon2 = resample(PCon, factor,1);
+    f2 = (0:length(PCon2)-1)*Fs/Frames/nppt/factor; %frequency resampled
+    times5(i) = f2(zerocrossPPT(PCon2(2:end))); %calculate zero cross (blind f_b)
+    plot(f2,PCon2)
+    xlabel('Frequency (Hz)')
+    ylabel('Phase Contrast (rad)')
+    hold all
+    title('Phase contrasts for different dephts ')
+    xlim([min(f) max(f) ])
 end
+figure(6),legend('0.2 mm', '0.4 mm', '0.6 mm','0.8 mm','1.0 mm','1.0 mm','1.2 mm','1.4 mm','1.6 mm','1.8 mm')
+times3_2(8) = exp(0.775)/0.018;
 
-%%
+%% ****** TSR ***********
 step = 0.018;
-limit = 5;
+limit = 4;
 depths = 0.2:0.2:1.8;
 t1 = times(1:limit)*step;
 figure, plot(t1, depths(1:limit),'xr', 'linewidth',1.5,'MarkerSize',10)
-set(get(gca,'xlabel'),'string','Specific characteristic time (s)','fontsize',12)
-set(get(gca,'ylabel'),'string','Depth (mm)','fontsize',12)
+set(get(gca,'xlabel'),'string','Specific characteristic time t (s)','fontsize',12)
+set(get(gca,'ylabel'),'string','Depth z (mm)','fontsize',12)
 set(get(gca,'title'),'string','Depth estimation','fontsize',11, 'FontWeight','bold')
 
 degree = 2;
 [p,s] = polyfit(t1', depths(1:limit)',degree);
-t2 = linspace(3.5,11.5);
+%t2 = linspace(3.5,11.5);
+t2 = linspace(1,9);
 z = polyval(p,t2);
 hold on
-h1 = plot(t2,z,'r');
-R = zeros(1,3);
+h1 = plot(t2,z,'r','linewidth',1.5,'markersize',3);
+R = zeros(1,5);
 R(1) =  (1 - s.normr^2/norm(depths(1:limit)-mean(depths(1:limit)))^2)*100;
-Zestimated = zeros(3,length(depths));
+Zestimated = zeros(5,length(depths));
 Zestimated(1,:) = polyval(p,times*step);
 p_tsr = p;
 
 
-t1 = times2*step;
+% ****** NTC ***********
+limit = 7;
+depths = depths(1:limit);
+t1 = times2(1:limit)*step;
 plot(t1, depths,'ob', 'linewidth',1.5,'MarkerSize',7)
 [p,s] = polyfit(t1(~isnan(t1))', depths(~isnan(t1))',1);
 t2 = linspace(0,10.5);
@@ -162,43 +228,122 @@ z = polyval(p,t2);
 hold on
 h2 = plot(t2,z,'.-.b','linewidth',1.5,'markersize',3);
 R(2) =  (1 - s.normr^2/norm(depths-mean(depths))^2)*100;
-Zestimated(2,:) = polyval(p,t1);
+Zestimated(2,:) = polyval(p,times2*step);
 p_ntc = p;
+depths = 0.2:0.2:1.8;
 
-
-linear = 0; %linear fit or not linear (sqrt(x))
-t1 = times3*step;
+% ****** OPD ***********
+%limit = 8;
+depths = depths(1:limit);
+linear = 1; %linear fit or not linear (sqrt(x))
+t1 = times3(1:limit)*step;
 plot(t1, depths,'+k', 'linewidth',1.5,'MarkerSize',7)
-t2 = linspace(0.4,10);
+t2 = linspace(0.0,11);
 if (linear)
     [p,s] = polyfit(t1', depths',1);
     z = polyval(p,t2);
     R(3) =  (1 - s.normr^2/norm(depths-mean(depths))^2)*100;
-    Zestimated(3,:) = polyval(p,t1);
+    Zestimated(3,:) = polyval(p,times3*step);
+    p_opd = p;
 else
     Eqn = 'a*sqrt(x) + b';
     [f1, gof] = fit(t1',depths',Eqn,'Startpoint',[1 1]);
     z = f1.a*sqrt(t2)+f1.b;
     R(3) = gof.rsquare*100;
-    Zestimated(3,:) = f1.a*sqrt(t1)+f1.b;
+    Zestimated(3,:) = f1.a*sqrt(times3*step)+f1.b;
+    p_opd(1) = f1.a;
+    p_opd(2) = f1.b;
 end
 hold on
 h3 = plot(t2,z,':k','linewidth',2);
+depths = 0.2:0.2:1.8;
 
 
-text(t1(1),depths(end)-0.4,['R^2 = ', num2str(R(3)), ' %'],'Color','black')
-text(t1(1),depths(end)-0.2,['R^2 = ', num2str(R(2)), ' %'],'Color','blue')
-text(t1(1),depths(end),['R^2 = ', num2str(R(1)), ' %'],'Color','red')
-legend([h1 h2 h3],'TSR', 'NTC', 'OPD','Location','southeast')
+% ****** PCT ***********
+%limit = 8;
+depths = depths(1:limit);
+linear = 1; %linear fit or not linear (sqrt(x))
+t1 = times4(1:limit)*step;
+plot(t1, depths,'*g', 'linewidth',1.5,'MarkerSize',7)
+t2 = linspace(0,11);
+if (linear)
+    [p,s] = polyfit(t1', depths',1);
+    z = polyval(p,t2);
+    R(4) =  (1 - s.normr^2/norm(depths-mean(depths))^2)*100;
+    Zestimated(4,:) = polyval(p,times4*step);
+    p_pca = p;
+else
+    Eqn = 'a*sqrt(x) + b';
+    [f2, gof] = fit(t1',depths',Eqn,'Startpoint',[1 1]);
+    z = f2.a*sqrt(t2)+f2.b;
+    R(4) = gof.rsquare*100;
+    Zestimated(4,:) = f2.a*sqrt(times4*step)+f2.b;
+    p_pca(1) = f2.a;
+    p_pca(2) = f2.b;
+end
+hold on
+h4 = plot(t2,z,'--g','linewidth',1.5,'markersize',3);
+depths = 0.2:0.2:1.8;
 
 
+% **** PPT *************
+range = 1:7;
+t = times5(range).^(-1);
+plot(t, depths(range),'^m', 'linewidth',1.5,'MarkerSize',7)
+[f1,gof] = fit(t',depths(range)','a*sqrt(x) + b');
+t2 = linspace(0,6);
+z = f1.a*sqrt(t2) + f1.b;
+h5 = plot(t2,z,'m');
+R(5) =  gof.rsquare*100;
+p_ppt = [f1.a f1.b];
+Zestimated(5,1:length(times5)) = p_ppt(1)*sqrt(times5.^-1) + p_ppt(2);
+
+%round to 1 and 2 decimals
+R = (round(10*R)/10);
+p_pca = (round(1000*p_pca)/1000);
+p_tsr = (round(1000*p_tsr)/1000);
+p_ntc = (round(1000*p_ntc)/1000);
+p_opd = (round(1000*p_opd)/1000);
+p_ppt = (round(1000*p_ppt)/1000);
+
+
+% ----Place equations and errors R^2
+depths = 0.2:0.2:1.8;
+signn = containers.Map({1, -1},{'+','-'});
+
+t1(1)=t1(1)-0.2;
+%TSR
+text(t1(1),depths(end)+0.1,['R^2_{TSR} = ', num2str(R(1)), ' %'],'Color','red','FontSize',8)
+text(t1(1),depths(end),['z_{TSR} = ', num2str(p_tsr(1)), 't^2 ', ...
+    signn(sign(p_tsr(2))),' ', num2str(abs(p_tsr(2))), 't ', ...
+    signn(sign(p_tsr(3))),' ',  num2str(abs(p_tsr(3))) ],'Color','red','FontSize',8)
+%NTC
+text(t1(1),depths(end)-0.15,['R^2_{NTC} = ', num2str(R(2)), ' %'],'Color','blue','FontSize',8)
+text(t1(1),depths(end)-0.25,['z_{NTC} = ', num2str(p_ntc(1)), 't ', ...
+    signn(sign(p_ntc(2))),' ',  num2str(abs(p_ntc(2))) ],'Color','blue','FontSize',8)
+%OPD
+text(t1(1),depths(end)-0.4,['R^2_{OPD} = ', num2str(R(3)), ' %'],'Color','black','FontSize',8)
+text(t1(1),depths(end)-0.5,['z_{OPD} = ', num2str(p_opd(1)), 't ', ...
+    signn(sign(p_opd(2))),' ',  num2str(abs(p_opd(2))) ],'Color','black','FontSize',8)
+%PCT
+text(t1(1),depths(end)-0.65,['R^2_{PCT} = ', num2str(R(4)), ' %'],'Color','green','FontSize',8)
+text(t1(1),depths(end)-0.75,['z_{PCT} = ', num2str(p_pca(1)), 't ', ...
+    signn(sign(p_pca(2))),' ',  num2str(abs(p_pca(2))) ],'Color','green','FontSize',8)
+% PPT
+text(t1(1),depths(end)-0.9,['R^2_{PPT} = ', num2str(R(5)), ' %'],'Color','m','FontSize',8)
+text(t1(1),depths(end)-1.0,['z_{PPT} = ', num2str(p_ppt(1)), 't^{1/2} '],'Color','m','FontSize',8)
+
+legend([h1 h2 h3 h4 h5],'TSR', 'NTC', 'OPD','PCT', 'PPT','Location','southeast')
+axis( [0 6 0.1 1.99])
+
+% Calculate Errors and print them
 Zestimated;
-Zexact = [depths;depths;depths];
-Zerror = (100*abs(Zestimated-Zexact)./Zexact);
+Zexact = [depths;depths;depths;depths;depths];
+Zerror = (100*(round(100*Zestimated)/100-Zexact)./Zexact);
 clc
 disp('*********** Predicted values (mm) *******')
-disp('Value     TSR         NTC         OPD')
-disp(num2str([depths' Zestimated']))
-disp('*********** Relative errors (%) *******')
-disp('  TSR           NTC           OPD')
-disp(num2str(Zerror'))
+disp('Value        TSR          NTC          OPD          PCT          PPT')
+disp(num2str([depths' Zestimated'],'%0.2f        '))
+disp('*********** Relative errors (%) *********')
+disp('             TSR          NTC          OPD          PCT          PPT')
+disp(num2str([nan*ones(1,size(Zerror,2))' Zerror'],'%0.1f       '))
